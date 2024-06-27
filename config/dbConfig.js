0a22c32d2d2c8f3a95a470eb1882d97908dfc005
ajith1506 <ajithkumararun1111@gmail.com>
2024-06-27 10:02:37 +0530

    change

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 //Connect to MongoDB
 const connectDB = async () => {
-  await mongoose.connect(process.env.DB_HOST, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(process.env.DB_HOST);
   console.log("MongoDB Connected...");
 };
 
