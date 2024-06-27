ca06d6c15ce1b677f7f0c7e6da757dbac7651c03
ajith1506 <ajithkumararun1111@gmail.com>
2024-06-27 17:46:28 +0530

    change

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -7,8 +7,8 @@ dotenv.config();
 // MongoDB connection function
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
-    console.log("MongoDB Connected...");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected succesfully...");
   } catch (err) {
     console.log("Error connecting to MongoDB:", err.message);
     // Exit process with failure
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connectDB = require("./config/dbConfig.js");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(bodyParser.json());
 
