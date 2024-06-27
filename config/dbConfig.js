105fbb54953017a2f5ca04fd873b25d73b2a0db7
ajith1506 <ajithkumararun1111@gmail.com>
2024-06-27 20:15:26 +0530

    change

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,18 +1,12 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 
-// Load environment variables from .env file
-dotenv.config();
-
-// MongoDB connection function
+//Connect to MongoDB
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected succesfully...");
-  } catch (err) {
-    console.log("Error connecting to MongoDB:", err.message);
-    // Exit process with failure
-  }
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("MongoDB Connected...");
 };
 
 module.exports = connectDB;
